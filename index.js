934fcc65da8ae244c2a9bae74f02c002d2a26581
o2hope <agusvaldes2@live.com>
2024-05-19 14:27:50 -0600

    Button fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const sendButton = document.querySelector('#enviar')
 
-const inputMail = document.querySelector('#correo');
+const inputMail = document.querySelector('#mail');
 
 
 const sending = (e) => {
-    if(e.target.value.length > 4) {
+    if(e.target.value.length > 4 && e.target.value.includes('@') && e.target.value.includes('.')){
         sendButton.disabled = false
         sendButton.innerHTML = ' (=‐ω‐=)'
     }else {
